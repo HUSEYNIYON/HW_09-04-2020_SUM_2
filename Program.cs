06d388610ff8cfe3e4936e8b260e1b66a59e85d7
Huseyn <your>
2020-04-10 15:53:53 +0300

    Made a Class with his interface inheritance and with methods

--- Program.cs
+++ Program.cs
@@ -11,15 +11,18 @@ namespace HomeWork
     }
     interface IPlayable 
     {
-        void Play();
-        void Pause();
-        void Stop();
+        void Play();  void Pause(); void Stop();
     }
     interface IRecodable
     {
-        void Record();
-        void Pause();
-        void Stop();
+        void Record();  void Pause(); void Stop();
     }
-   
+    public class Player: IPlayable, IRecodable
+    {
+        public void Play() => Console.WriteLine("Play the tune!");
+        public void Pause() => Console.WriteLine("Pause the tune!");
+        public void Stop() => Console.WriteLine("Stop the tune!");
+        public void Record() => Console.WriteLine("Record the tune!");
+
+    }
 }

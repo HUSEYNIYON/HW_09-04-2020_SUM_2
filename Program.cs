1a4bcfb0351310221d81b2ced92758db25989df3
Huseyn <your>
2020-04-10 15:36:52 +0300

    Made two interface with three methods

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    interface IPlayable 
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+   
+}
